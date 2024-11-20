f3f555388775b96de512498df7362275986b81b4
Rimao416 <kayumbaomari27@gmail.com>
2024-11-20 14:55:07 +0100

    05_useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
+"use client";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  console.log(searchParams.get("search"))
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href="/dashboard#yellow">Dashboard Yello part</Link>
-      <Link href="/dashboard#green">Dashboard Green part</Link>
-      <Link href="/dashboard#red">Dashboard Red part</Link>
     </div>
   );
 }
