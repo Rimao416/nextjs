613faf094bebf036eabe7b24ef9a44fc81b7a59b
Rimao416 <kayumbaomari27@gmail.com>
2024-11-20 14:18:06 +0100

    02_Pathname

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,31 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function Header() {
+  const pathname = usePathname();
   return (
     <div>
       <ul id="nav">
         <li className="nav-link">
           <Link href={"/"}>
-            <h1 data-name="home">Home</h1>
+            <h1 data-name="home" className={pathname === "/" ? "active" : ""}>Home</h1>
           </Link>
         </li>
         <li className="nav-link">
           <Link href={"/dashboard"}>
-            <h1 data-name="dashboard">Dashboard</h1>
+            <h1 data-name="dashboard" className={pathname === "/dashboard" ? "active" : ""}>Dashboard</h1>
           </Link>
         </li>
         <li className="nav-link">
           <Link href={"/blog"}>
-            <h1 data-name="blog">Blog</h1>
+            <h1 data-name="blog" className={pathname === "/blog" ? "active" : ""}>Blog</h1>
           </Link>
         </li>
         <li className="nav-link">
           <Link href={"/dashboard/settings"}>
-            <h1 data-name="settings">Settings</h1>
+            <h1 data-name="settings" className={pathname === "/dashboard/settings" ? "active" : ""}>Settings</h1>
           </Link>
         </li>
       </ul>
