e675f1858d15ba7bdf8de32a6c5d1da6dbaf83f7
Rimao416 <kayumbaomari27@gmail.com>
2024-11-20 14:44:50 +0100

    04_UseRouter

--- components/header.tsx
+++ components/header.tsx
@@ -1,34 +1,57 @@
 "use client";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function Header() {
   const pathname = usePathname();
+  const router = useRouter();
   return (
     <div>
       <ul id="nav">
         <li className="nav-link">
-          <Link href={"/"}>
-            <h1 data-name="home" className={pathname === "/" ? "active" : ""}>Home</h1>
-          </Link>
+          <span onClick={() => router.push("/")}>
+            <h1 data-name="home" className={pathname === "/" ? "active" : ""}>
+              Home
+            </h1>
+          </span>
         </li>
         <li className="nav-link">
-          <Link href={"/dashboard"}>
-            <h1 data-name="dashboard" className={pathname === "/dashboard" ? "active" : ""}>Dashboard</h1>
-          </Link>
+          <span onClick={() => router.push("/dashboard")}>
+            <h1
+              data-name="dashboard"
+              className={pathname === "/dashboard" ? "active" : ""}
+            >
+              Dashboard
+            </h1>
+          </span>
         </li>
         <li className="nav-link">
-          <Link href={"/blog"}>
-            <h1 data-name="blog" className={pathname === "/blog" ? "active" : ""}>Blog</h1>
-          </Link>
+          <span onClick={() => router.push("/blog")}>
+            <h1
+              data-name="blog"
+              className={pathname === "/blog" ? "active" : ""}
+            >
+              Blog
+            </h1>
+          </span>
         </li>
         <li className="nav-link">
-          <Link href={"/dashboard/settings"}>
-            <h1 data-name="settings" className={pathname === "/dashboard/settings" ? "active" : ""}>Settings</h1>
-          </Link>
+          <span onClick={() => router.push("/dashboard/settings")}>
+            <h1
+              data-name="settings"
+              className={pathname === "/dashboard/settings" ? "active" : ""}
+            >
+              Settings
+            </h1>
+          </span>
         </li>
       </ul>
+      <div style={{marginTop:"50px",display:"flex",columnGap:"20px"}}> 
+
+      <button onClick={() => router.back()}>Back</button>
+      <button onClick={() => router.forward()}>Forward</button>
+      </div>
     </div>
   );
 }
