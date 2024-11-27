c37c64201aa1f50cbb6b9a17ecd6ebff0ec13274
Rimao416 <kayumbaomari27@gmail.com>
2024-11-27 10:45:51 +0100

    09_Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import sleep from "sleep-promise";
+export default async function Home() {
+    const res=await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness',{
+      headers: {
+        'X-Api-Key': 'pDsBb6dih1wbK+uRBQv9fQ==EvLVJpOpcGonr9ex',
+      },
+      next: { revalidate: 0 },
+    }).then((res) => res.json()).then(sleep(2000));
+
     return (
-      <div>
-       <h1>Je suis le Header de toutes les pages</h1>
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>
+        {res.map((quote) => (
+          <p key={quote.id}>{quote.quote}</p>
+        ))}
+        
       </div>
     );
   }
